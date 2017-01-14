28571610da2d6fafdb2d2f071b3fd5693e31e9c3
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-01-13 17:37:56 -0800

    POST, DELETE endpoints

--- shopping-list/server.js
+++ shopping-list/server.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+// var indexHTMLfile = require('./public/index.html')
+
 
 var Storage = {
     add: function (name) {
@@ -9,6 +13,12 @@ var Storage = {
         this.items.push(item);
         this.setId += 1;
         return item;
+    },
+
+    delete: function (id) {
+
+        this.items = this.items.filter(item => item.id != id);
+        return this.items;
     }
 };
 
@@ -30,6 +40,27 @@ app.use(express.static('public'));
 
 app.get('/items', function (request, response) {
     response.json(storage.items);
+});
+
+
+// add the POST route (endpoint)
+app.post('/items', jsonParser, function (request, response) {
+    // if the name property from request body is missing:
+    if (!('name' in request.body)) {
+        return response.sendStatus(400);
+    }
+    // if it is not missing you just add the item to the shoppin list and return 201 created.
+    var item = storage.add(request.body.name);
+    response.status(201).json(item);
+});
+
+// add the DELETE endpoint or route
+
+app.delete('/items/:id', jsonParser, function (request, response) {
+    var id = request.params.id;
+
+    storage.delete(id);
+    response.status(200).json(id);
 });
 
 // run on local host 8080
