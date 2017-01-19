24379a23b692de5bf41a156afbdc7dbfb96d04a2
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-01-19 12:24:29 -0800

    mongoose CRUD

--- snippets-mongoose/snippets.js
+++ snippets-mongoose/snippets.js
@@ -17,67 +17,85 @@ mongoose.connection.once('open', function () {
     });
 
     var Snippet = mongoose.model('Snippet', snippetSchema);
-});
 
 
-// CRUD operations
+    // CRUD operations
 
-var create = function (name, content) {
-    var snippet = {
-        name: name,
-        content: content
+    var create = function (name, content) {
+        var snippet = {
+            name: name,
+            content: content
+        };
+        Snippet.create(snippet, function (err, snippet) {
+            if (err || !snippet) {
+                console.error("Could not create snippet", name);
+                mongoose.disconnet();
+                return;
+            }
+            console.log("Created snippet", snippet.name);
+            mongoose.disconnet();
+        });
     };
-    Snippet.create(snippet, function (err, snippet) {
-        if (err || !snippet) {
-            console.error("Could not create snippet", name);
+
+    var read = function (name) {
+        Snippet.findOne({
+            name: name
+        }, function (err, snippet) {
+            if (err || !snippet) {
+                console.error("Could not read snippet", name);
+                mongoose.disconnet();
+                return;
+            }
+            console.log("Updated snippet", snippet.name);
             mongoose.disconnet();
-            return;
-        }
-        console.log("Created snippet", snippet.name);
-        mongoose.disconnet();
-    });
-};
+        });
+    };
 
-var read = function (name) {
-    Snippet.findOne({
-        name: name
-    }, function (err, snippet) {
-        if (err || !snippet) {
-            console.error("Could not read snippet", name);
-            mongoose.disconnet();
-            return;
-        }
-        console.log("Updated snippet", snippet.name);
-        mongoose.disconnet();
-    });
-};
-
-var update = function (name, content) {
-    Snippet.findOneAndUpdate({
-        name: name
-    }, {
-        content: content
-    }, function (err, snippet) {
-        if (err || !snippet) {
-            console.error("Could not update snippet", name);
+    var update = function (name, content) {
+        Snippet.findOneAndUpdate({
+            name: name
+        }, {
+            content: content
+        }, function (err, snippet) {
+            if (err || !snippet) {
+                console.error("Could not update snippet", name);
+                mongoose.disconnect();
+                return;
+            }
+            console.log("Updated snippet", snippet.name);
             mongoose.disconnect();
-            return;
-        }
-        console.log("Updated snippet", snippet.name);
-        mongoose.disconnect();
-    });
-};
+        });
+    };
 
-var del = function (name, content) {
-    Snippet.findOneAndRemove({
-        name: name
-    }, function (err, snippet) {
-        if (err || !snippet) {
-            console.error("Could not delete snippet", name);
+    var del = function (name, content) {
+        Snippet.findOneAndRemove({
+            name: name
+        }, function (err, snippet) {
+            if (err || !snippet) {
+                console.error("Could not delete snippet", name);
+                mongoose.disconnect();
+                return;
+            }
+            console.log("Deleted snippet", snippet.name);
             mongoose.disconnect();
-            return;
+        });
+    };
+
+    var main = function () {
+        if (process.argv[2] == 'create') {
+            create(process.argv[3], process.argv[4]);
+        } else if (process.argv[2] == 'read') {
+            read(process.argv[3]);
+        } else if (process.argv[2] == 'update') {
+            update(process.argv[3], process.argv[4]);
+        } else if (process.argv[2] == 'delete') {
+            del(process.argv[3]);
+        } else {
+            console.error('Command not recognized');
+            mongoose.disconnect();
         }
-        console.log("Deleted snippet", snippet.name);
-        mongoose.disconnect();
-    });
-};
+    }
+
+    main();
+
+});
